9a67b4bcf4c742dab34dc6aa8620b398b36c88b4
vivekChenna <chennavivek96@gmail.com>
2023-09-08 14:20:06 +0530

    completed authentication service

--- src/middlewares/UserMiddleWare.js
+++ src/middlewares/UserMiddleWare.js
@@ -1,7 +1,9 @@
- exports.CheckDetails = (req, res , next) => {
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-    console.log(req.body.email);
-    console.log(req.body.password);
+exports.CheckDetails = (req, res, next) => {
+  console.log(req.body.email);
+  console.log(req.body.password);
   if (!req.body.password || !req.body.email) {
     return res.status(400).json({
       message: "please fill all the details",
@@ -9,4 +11,85 @@
   }
 
   next();
+};
+
+exports.Auth = (req, res, next) => {
+  try {
+    const { token } = req.body;
+
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        message: "Token Missing",
+      });
+    }
+
+    // now if token is present then
+
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("printing decode");
+      req.user = decode;
+      console.log(decode); // now we will send the decoded payload in the req object
+    } catch (error) {
+      console.log("something went wrong while verifying JwT Token");
+      console.log(error);
+
+      return res.status(500).json({
+        success: false,
+        message: "something went wrong while verifying JWT Token",
+      });
+    }
+
+
+    console.log("printing req.user");
+    console.log(req.user);
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "something went wrong ",
+      err: { error },
+    });
+  }
+};
+
+exports.isStudent = (req, res, next) => {
+  try {
+    if (req.user.role !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for Students",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log("something went wrong ");
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User role is not matching ",
+    });
+  }
+};
+
+exports.isAdmin = (req, res, next) => {
+  try {
+    if (req.user.role !== "Admin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for Admin",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log("something went wrong ");
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User role is not matching ",
+    });
+  }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const { signUp, login } = require("../controllers/UserController");
-const { CheckDetails } = require("../middlewares/UserMiddleWare");
+const {
+  CheckDetails,
+  isStudent,
+  isAdmin,
+  Auth,
+} = require("../middlewares/UserMiddleWare");
 
 router.post("/signup", signUp);
 router.post("/login", CheckDetails, login);
+router.post("/student", Auth, isStudent, (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Welcome to the Protected Routes for Students",
+  });
+});
+
+router.post("/admin", Auth, isAdmin, (req, res) => {
+  return res.status.json({
+    success: true,
+    message: "Welcome to the protected routes for Admin",
+  });
+});
 
 module.exports = router;
